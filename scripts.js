f0c854aee757e5e627a7c97be1a6dbaec95c2398
Arthur948-coder <arthurucho9@gmail.com>
2025-07-05 22:54:04 -0300

    Responsividade, Pagina de Login e botao whatsapp

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
 function toggleDarkMode() {
     document.body.classList.toggle('dark-mode');
 }
+
+const form = document.querySelector(".formulario-login")
+const mascara = document.querySelector(".mascara-formulario")
+
+function login(){
+    form.style.visibility = "visible"
+}
+
+function fecharformulario(){
+    mascara.style.visibility = "hidden"
+    form.style.visibility = "hidden"
+}
