a9e217e7c73f6668ee07258c545a6ea55e46ad0d
Arthur948-coder <arthurucho9@gmail.com>
2025-07-14 17:45:22 -0300

    Pagina Cadastro, botao de entrar no carrinho

--- scripts.js
+++ scripts.js
@@ -1,9 +1,6 @@
-function toggleDarkMode() {
-    document.body.classList.toggle('dark-mode');
-}
-
 const form = document.querySelector(".formulario-login")
 const mascara = document.querySelector(".mascara-formulario")
+const form2 = document.querySelector(".formulario-cadastro")
 
 function login(){
     form.style.visibility = "visible"
@@ -12,4 +9,18 @@ function login(){
 function fecharformulario(){
     mascara.style.visibility = "hidden"
     form.style.visibility = "hidden"
+}
+
+function cadastro(){
+    form.style.visibility = "hidden"
+    form2.style.visibility = "visible"
+}
+
+function voltarParaLogin(){
+    form2.style.visibility = "hidden"
+    form.style.visibility = "visible"
+}
+
+function fecharform2(){
+    form2.style.visibility = "hidden"
 }
